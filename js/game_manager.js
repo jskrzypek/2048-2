df79919b67e7946a3fa44b67d4e77e68a27f3239
Joshua Skrzypek <jskrzypek@gmail.com>
2014-03-16 18:21:30 +0200

    Changed goal

--- js/game_manager.js
+++ js/game_manager.js
@@ -124,7 +124,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 9007199254740992) self.won = true;
+          if (merged.value === 4194304) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
